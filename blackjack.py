f91bfa08b9ca654eb5160083e3435bb8575d030a
FraserRowan <97613287+FraserRowan@users.noreply.github.com>
2022-01-20 10:38:20 +0000

    fixed blackjack

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,7 @@
 import random
 import sys
 total= 0
+dealertotal = random.randint(7,20)
 if total >= 21:
     print("you lose")
 else:
@@ -17,14 +18,18 @@ if hit_fold == "hit":
     total = total+card2
     print("your current total is",total)
 elif hit_fold == "fold":
-    print(total)
-    print("you win")
-    exit()
-    
-else :
-    print("please enter hit or fold")
+    if total > dealer :
+        print("your total was", total, "the dealers total was", dealertotal, "you win")
+    elif dealertotal > total:
+        print("your total was",total,"dealer total was",dealertotal,"you lose")
+    else :
+        print("your total was",total,"dealer total was",dealertotal,"you drew")
+        
+
+print("please enter hit or fold")
 if total >= 21:
     print("you lose")
+    exit()
 else:
     print("its not over yet")
 
@@ -32,16 +37,21 @@ hit_fold = input("would you like to hit or fold? ")
 if hit_fold == "hit":
     card3 = random.randint(1,12)
     print("your third question is",card3)
-    total = total+card2
+    total = total+card3
     print("your current total is",total)
 elif hit_fold == "fold":
-    print(total)
-    print("you win")
-    exit()
-else :
-    print("please enter hit or fold")
+    if total > dealertotal :
+        print("your total was", total, "the dealers total was", dealertotal, "you win")
+    elif dealertotal > total:
+        print("your total was",total,"dealer total was",dealertotal,"you lose")
+    else :
+        print("your total was",total,"dealer total was",dealertotal,"you drew")
+exit()
+       
+print("please enter hit or fold")
 if total >= 21:
     print("you lose")
+    exit()
 else:
     print("its not over yet")
 
@@ -49,16 +59,22 @@ hit_fold = input("would you like to hit or fold? ")
 if hit_fold == "hit":
     card4 = random.randint(1,12)
     print("your fourth card is",card4)
-    total = total+card2
+    total = total+card4
     print("your current total is",total)
 elif hit_fold == "fold":
-    print(total)
-    print("you win")
-    exit()
-else :
-    print("please enter hit or fold")
+    if total > dealertotal :
+        print("your total was", total, "the dealers total was", dealertotal, "you win")
+    elif dealertotal > total:
+        print("your total was",total,"dealer total was",dealertotal,"you lose")
+    else :
+        print("your total was",total,"dealer total was",dealertotal,"you drew")
+exit()
+       
+        
+print("please enter hit or fold")
 if total >= 21:
     print("you lose")
+    exit()
 else : 
     print("its not over yet")
 
@@ -66,16 +82,23 @@ hit_fold = input("would you like to hit or fold? ")
 if hit_fold == "hit":
     card5 = random.randint(1,12)
     print("your fith card is",card5)
-    total = total+card2
+    total = total+card5
     print("your current total is",total)
 elif hit_fold == "fold":
-    print(total)
-    print("you win")
-    exit()
-else :
-    print("please enter hit or fold")
+    if total > dealertotal :
+        print("your total was", total, "the dealers total was", dealertotal, "you win")
+    elif dealer > total :
+        print("your total was",total,"dealer total was",dealertotal,"you lose")
+    else :
+        print("your total was",total,"dealer total was",dealertotal,"you drew")
+else:
+    print("its not over yet")
+exit()
+
+print("please enter hit or fold")
 if total >= 21:
     print("you lose")
+    exit()
 else : 
     print("its not over yet")
     
@@ -83,16 +106,22 @@ hit_fold = input("would you like to hit or fold? ")
 if hit_fold == "hit":
     card6 = random.randint(1,12)
     print("your sixth card is",card6)
-    total = total+card2
+    total = total+card6
     print("your current total is",total)
 elif hit_fold == "fold":
-    print(total)
-    print("you win")
-    
+    if total > dealertotal :
+        print("your total was", total, "the dealers total was", dealertotal, "you win")
+    elif dealertotal > total:
+        print("your total was",total,"dealer total was",dealertotal,"you lose")
+    else :
+        print("your total was",total,"dealer total was",dealertotal,"you drew")
+    exit()
 else :
     print("please enter hit or fold")
 if total >= 21:
-    print("you lose")
+    print(
+        "you lose")
+    exit()
 else : 
     print("its not over yet")
 
@@ -103,11 +132,18 @@ if hit_fold == "hit":
     total = total+card7
     print("your current total is",total)
 elif hit_fold == "fold":
-    print(total)
-    print("you win")
-else :
-    print("please enter hit or fold")
+    if total > dealertotal :
+        print("your total was", total, "the dealers total was", dealertotal, "you win")
+    elif dealertotal > total:
+        print("your total was",total, "dealer total was",dealertotal, " you lose")
+    else :
+        print("your total was",total, "dealer total was",dealertotal, "you drew")
+        exit()
+else:
+    print("its not over yet")
+print("please enter hit or fold")
 if total >= 21:
     print("you lose")
+    exit()
 else : 
     print("its not over yet")
